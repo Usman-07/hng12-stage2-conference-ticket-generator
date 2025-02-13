02b4403b90832e4ca3449bc68ffde7c608cd9e10
Al-Ameen Usman <al-ameen.usman@ubagroup.com>
2025-02-13 23:14:39 +0100

    Adjusted mobile responsiveness

--- src/Components/TicketNav.jsx
+++ src/Components/TicketNav.jsx
@@ -7,7 +7,7 @@ export const TicketNav = () => {
     return (
         <>
             <div className='border border-[#197686] py-3 px-1 lg:px-6 rounded-2xl my-3 bg-[#08252B] jeju-font'>
-              <nav className='flex space-x-36 lg:space-x-32 md:space-x-32'>
+              <nav className='flex space-x-32 lg:space-x-32 md:space-x-32'>
                 <div className='flex flex-shrink-0'>
                     <img src={LogoFrame} alt="ticketLogo" className='px-1 ml-2' />
                 </div>
